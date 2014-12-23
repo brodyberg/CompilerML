0a470d170f611ed457c198ea8adf51f7e158ef04
brodyberg <brodyberg@gmail.com>
2014-12-23 15:40:44 -0800

    Expression compiles

--- MaxArgs.fs
+++ MaxArgs.fs
@@ -1,50 +1,46 @@
 # load "StraightlineTree.fs"
-open ChapterOne
-open Straightline
 
 // Example program: 
 // a := 5 + 3 ; b := ( print ( a , a - 1 ) , 10 * a ) ; print ( b )
 
-//let x = Id("foo")
-
-//let x = CompoundStatement(1)
-
-let x = foo() // no
-let x = Straightline.foo() // no
-let x = ChapterOne.Straightline.foo() // yes
-
-open ChapterOne
-
-let x = foo() // no
-let x = Straightline.foo() // yes
-let x = ChapterOne.Straightline.foo() // yes
-
 open ChapterOne
 open Straightline
 
-let x = foo() // yes
-let x = Straightline.foo() // yes
-let x = ChapterOne.Straightline.foo() // yes
+//let x = foo() // yes
+//let x = Straightline.foo() // yes
+//let x = ChapterOne.Straightline.foo() // yes
+
+let x = Id("foo")
+//let x = AssignmentStatement(Id("foo"), Expression(NumberExpression(5)))
+let x = AssignmentStatement(Id("foo"), NumberExpression(5))
+
+
+
+let x = Plus
+
+// we can see Id & Binop, but not Statement or Expression
+let Expression
 
 let prog = 
     CompoundStatement(
         AssignmentStatement(
-            "a", 
+            Id("a"), 
             OperatorExpression(
                 NumberExpression 5,
                 Plus,
                 NumberExpression 3)),
         CompoundStatement(
-            AssignmentStatement("b",
+            AssignmentStatement(
+                Id("b"),
                 EseqExpression(
-                    PrintStatement [IdExpression "a";
+                    PrintStatement [IdExpression(Id("a"));
                                     OperatorExpression(
-                                        IdExpression "a", 
+                                        IdExpression(Id("a")), 
                                         Minus,
                                         NumberExpression 1);],
                     OperatorExpression(
                         NumberExpression 10,
                         Times,
-                        IdExpression "a"))),
-            PrintStatement [IdExpression "b" ]))
+                        IdExpression(Id("a"))))),
+            PrintStatement [IdExpression(Id("b")) ]))
 
