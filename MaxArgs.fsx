1022e9b7f9d73e02640cad08d25e86cc7974f6cb
brodyberg <brodyberg@gmail.com>
2014-12-24 19:36:33 -0800

    Fold can be tricky

--- MaxArgs.fsx
+++ MaxArgs.fsx
@@ -60,53 +60,93 @@ let p2 =
                         NumberExpression 1);]
 
 let rec visitStatement (statement:Statement) (acc:string list) =
+    printfn "0"
     match statement with
     | CompoundStatement(left,right) -> 
+        printfn "1"
         "CompoundStatement(\n" :: 
         (visitStatement left acc) @ 
         (visitStatement right acc) @ 
         ")" :: acc
     | AssignmentStatement(id,expression) -> 
+        printfn "2"
         "AssignmentStatement(" ::
         id :: 
         (visitExpression expression acc) @ 
         ")" :: acc
     | PrintStatement(expressionList) -> 
-        let start =     
-            "PrintStatement(\n" :: 
-            (expressionList
-            |> List.fold (fun acc2 expression -> 
-                printfn "expression: %A" expression
-                acc2 @ (visitExpression expression acc2))
-                [])
-        start @ ")" :: acc        
+        printfn "3"
+//        let start =     
+            // we're accumulating something
+            // and we're walking something
+            // make sure we're doing the right one at the
+            // right time
+//        let newThings =     
+//            expressionList
+//            |> List.map (fun expression ->
+//                visitExpression expression acc)
+
+        let foo =
+            expressionList
+            |> List.fold (fun expressionItems (item:Expression) ->
+                expressionItems @ (visitExpression item []) []
+//                (visitExpression item []) @ expressionItems) []
+            
+        foo @ acc
+
+//        "PrintStatement(\n" :: newThings @ ")" :: acc
+            
+//            
+//            (expressionList
+//            |> List.fold (fun acc2 expression -> 
+//                printfn "expression: %A" expression
+//                acc2 @ (visitExpression expression acc2))
+////                acc)
+//        start @ ")" :: acc        
 and visitExpression (expression:Expression) (acc:string list) = 
+    printfn "4"
     match expression with
     | IdExpression(id) -> 
-        printfn "id: %s" id
-        "IdExpression(" :: id :: ")" :: acc
+        printfn "5"
+        printfn "id: %s inList: %A" id acc
+
+        let item = sprintf "IdExpression(%s)" id
+
+//        let returnList = "IdExpression(" :: id :: ")" :: acc
+
+        let returnList = item :: acc
+
+        printfn "returnList: %A\n" returnList
+
+        returnList
 //        (visitId id acc) @ 
 //        ")" :: acc
     | NumberExpression(number) -> 
+        printfn "6"
         (visitNumber number acc) @ acc
     | OperatorExpression(left,binop,right) -> 
+        printfn "7"
         "OperatorExpression(" :: 
         (visitExpression left acc) @ 
         (visitBinop binop acc) @ 
         (visitExpression right acc) @ 
         ")" :: acc
     | EseqExpression(statement,expression) ->
+        printfn "8"
         "EseqExpression(\n" ::
         (visitStatement statement acc) @ 
         (visitExpression expression acc) @ 
         ")" :: acc
 and visitBinop binop (acc:string list) = 
+    printfn "9"
     match binop with
     | Plus -> "Plus" :: acc
     | Minus -> "Minus" :: acc
     | Times -> "Times" :: acc
     | Div -> "Div" :: acc
-and visitNumber number (acc:string list) = (number.ToString()) :: acc
+and visitNumber number (acc:string list) = 
+    printfn "10"    
+    (number.ToString()) :: acc
 //and visitId name (acc:string list) = name :: acc
 
 visitStatement prog []
@@ -122,6 +162,7 @@ visitStatement p6 []
 visitStatement p6a []
 |> List.iter (fun item -> printf "%s" item) 
 
+//System.String.Join
 
 // Write (maxargs: Statement -> int) that tells the maximum 
 // number of arguments of any print statement within any sub
