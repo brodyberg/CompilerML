a6d077f82eb61ef2407d91039b8e2865ab7ca71c
brodyberg <brodyberg@gmail.com>
2014-12-26 17:18:33 -0800

    Straightline Interpreter w/ side-effects

--- InterpretStatement.fsx
+++ InterpretStatement.fsx
@@ -29,23 +29,51 @@ let prog =
                         IdExpression "a"))),
             PrintStatement [IdExpression "b"]))
 
-// mutually recursive: 
-// interpStm(stm * table) -> table
-// interpExp(exp * table) -> int * table
-// update(table * id * int) -> table
-// lookup(table * id) -> int // searches down the list
-
-let update table id value = (id,value) :: table
-let lookup (table:((string*int) list)) (id:string) = 
+// Can we make this signature more awkward?
+let update id (thing:(int * ((string * int) list))) = (id,fst thing) :: snd thing
+let lookup (id:string) (table:((string*int) list)) = 
     snd (List.find (fun (pair:string * int) -> fst pair = id) table)
 
-// Note: case-sensitive
-let table = update [] "C" 6
-lookup table "C"
-let table' = update table "c" 7
-lookup table' "c"
-lookup table "C"
+let interpret (statement:Statement) = 
+    let rec interpStm (statement:Statement) (table:((string * int) list)) =
+        match statement with
+        | CompoundStatement(left,right) -> 
+            table
+            |> interpStm left
+            |> interpStm right
+        | AssignmentStatement(id,expression) -> 
+            update id (interpExp expression table)
+        | PrintStatement(expressionList) -> 
+            expressionList
+            |> List.fold (fun acc expression -> 
+                let (val',table') = interpExp expression acc
+                printfn "%d" val'
+                table') 
+                table
+    and interpExp (expression:Expression) (table:((string * int) list)) = 
+        match expression with
+        | IdExpression(id) -> lookup id table, table
+        | NumberExpression(number) -> number, table
+        | OperatorExpression(left,binop,right) -> 
 
+            let (valL,tableL) = interpExp left table
+            let (valR,tableR) = interpExp right tableL
 
+            match binop with
+            | Plus -> valL + valR, tableR
+            | Minus -> valL - valR, tableR
+            | Times -> valL * valR, tableR
+            | Div -> valL / valR, tableR
+        | EseqExpression(statement,expression) ->
+            table
+            |> interpStm statement
+            |> interpExp expression
 
-let interp (statement:Statement) = 0
+    interpStm statement []
+
+interpret prog
+
+//8
+//7
+//80
+//val it : (string * int) list = [("b", 80); ("a", 8)]
