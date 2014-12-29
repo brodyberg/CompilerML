0a85513a7ff2b6e56a86453f46c9d15e26751f5a
brodyberg <brodyberg@gmail.com>
2014-12-29 12:48:05 -0800

    Start Lex/Yacc work for Tiger

---
+++ Tiger/Tiger/AssemblyInfo.fs
@@ -0,0 +1,18 @@
+﻿module Tiger.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<AssemblyTitle("Tiger")>]
+[<AssemblyDescription("")>]
+[<AssemblyConfiguration("")>]
+[<AssemblyCompany("")>]
+[<AssemblyProduct("")>]
+[<AssemblyCopyright("brodyberg")>]
+[<AssemblyTrademark("")>]
+[<AssemblyVersion("1.0.0.0")>]
+()
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
---
+++ Tiger/Tiger/Component1.fs
@@ -0,0 +1,5 @@
+﻿namespace Tiger
+
+type Class1() = 
+    member this.X = "F#"
+
