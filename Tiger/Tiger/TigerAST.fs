2e0fcfd3bd6ddb641b9f0471e9838c72a240c476
brodyberg <brodyberg@gmail.com>
2014-12-30 14:27:30 -0800

    TigerLexer builds

--- Tiger/Tiger/TigerAST.fs
+++ Tiger/Tiger/TigerAST.fs
@@ -1,10 +1,20 @@
 namespace TigerAST
 
+// I think it all has to build to the same type overall - re: error below
 type value = 
     | Int of int
     | Float of float
     | String of string
+    | Comma
+    | EOF
 
 type Comment = 
     | CommentStart
     | CommentEnd
+
+///Tiger.fsl(37,34): error FS0001: This expression was expected to have type
+//    value    
+//but here has type
+//    Operator    
+//type Operator =
+//    | Comma
--- Tiger/Tiger/UseLexer.fsx
+++ Tiger/Tiger/UseLexer.fsx
@@ -9,16 +9,7 @@ let tigerQueens = File.ReadAllText "/Users/brodyberg/code/CompilerML/Tiger/Tiger
 
 let lexbuf = LexBuffer<_>.FromString tigerQueens
 
-// THIS file doesn't load TigerAST, but rather the fsl file needs to load it
-
-// #load "TigerAST.fs" // fixup TigerLexer.fs to get Tiger.fsl to pull right TigerAST
-#load "TigerAST.fs"
-// copy into generated lexer: open Microsoft.FSharp.Text.Lexing   
 #load "TigerLexer.fs"
-
-// This signature needs to be in the *.fsl so that it can be built into 
-// the TigerLexer.fs - and then so the call actually works at runtime
-TigerAST.Int(5)
 
 while not lexbuf.IsPastEndOfStream do  
     printfn "%A" (TigerLexer.tokenize lexbuf)
