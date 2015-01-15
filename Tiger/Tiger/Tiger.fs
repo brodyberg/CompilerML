f1bd6677a60b65c82517653ab456ce79587915bc
brodyberg <brodyberg@gmail.com>
2015-01-15 12:30:57 -0800

    Start Productions and AST

--- Tiger/Tiger/Tiger.fs
+++ Tiger/Tiger/Tiger.fs
@@ -1,7 +1,46 @@
 namespace Tiger
 
+type Op = 
+    | Set
+    | LBracket
+    | RBracket
+    | Plus
+    | Minus
+
+//type Statement = 
+  //  | Number of int
+
+type Label = 
+    | Id of string
+    | TypeName of string
+
+type PrimitiveType = 
+    | Int
+
+type HighLevelType = 
+    | Array
+    
+// I'm confused, do we need to put syntax level stuff in our AST?
+// like var row := intArray [ N ] of 0?
+
+// what we are starting to do below is create the productions
+// which should actually happen in fsp
+// also: incrementalism
+
 type Statement = 
-    | Number of int
+    | ArrayDecl of Label * 
+    | TypeDecl of Label * HighLevelType * PrimitiveType
+    // and then we specify the actual type?
+    | VarInt of Label * PrimitiveType * int
+    | VarType of Label * Label // length, initialValue
+    | Foo of string * string
+
+
+// program with statements
+
+
+type Program = 
+    | AST of 
 
 type AST = 
     | Let of Statement list
