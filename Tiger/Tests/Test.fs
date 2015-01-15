0d8addc6ee8d2b4aa27d50f7c8400e74a25d614e
brodyberg <brodyberg@gmail.com>
2015-01-14 18:54:31 -0800

    Toward unit tests

---
+++ Tiger/Tests/Test.fs
@@ -0,0 +1,23 @@
+﻿namespace Tests
+
+open System
+open NUnit.Framework
+
+open TigerLexer
+
+[<TestFixture>]
+module LexerTests = 
+
+    let tokenize str =
+        
+        let lexbuf = Lexing.LexBuffer<_>.FromString str    
+    
+        let _tokenize (lexbuf:Lexing.LexBuffer<char>) = 
+            while not lexbuf.IsPastEndOfStream do          
+                printfn "%A" (TigerLexer.tokenize lexbuf)
+    
+        _tokenize lexbuf
+
+    [<Test>]
+    let ``Tokenize simple Id``() = 
+        Assert.AreEqual("ID foo", "foo" |> tokenize)        
